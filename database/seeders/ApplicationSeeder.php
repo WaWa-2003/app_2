9c1506ef1fae9273704083b981ab135135eee9ea
WaWa-2003 <167841303+WaWa-2003@users.noreply.github.com>
2024-06-06 11:09:50 +0700

    adjusting the factories and data seeders according to the seperating wishlist controller and apply the application controller for the applicants

--- database/seeders/ApplicationSeeder.php
+++ database/seeders/ApplicationSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Application;
+use App\Models\Applicant\Application;
 
 class ApplicationSeeder extends Seeder
 {
--- database/seeders/WishlistSeeder.php
+++ database/seeders/WishlistSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Wishlist;
+use App\Models\Applicant\Wishlist;
 
 class WishlistSeeder extends Seeder
 {
